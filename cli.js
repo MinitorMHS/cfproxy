f49c18b21525492e92b4f4aa38c1b44d50442583
DNetL <57824119+DNetL@users.noreply.github.com>
2023-05-22 08:19:11 +0000

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-const { proxy, gen } = require('./index.js');
+const { proxy, gen, v } = require('./index.js');
 const cpath = './config.json';
 switch(process.argv[2]){
 case 'go': proxy(cpath); break;
 case 'gen': gen(cpath); break;
+case 'v': v(); break;
 default: 
 console.log(
 `Usage: cfproxy <option>
 
 Options:
-  gen 		generate the config
+  gen 		generate the socks5 config
   go 		start a socks5 proxy server
+  v 		generate the v2ray config
 
 cfproxy v0.0.1
 Copyright (c) 2023 DNetL <DNetL@pm.me>;
